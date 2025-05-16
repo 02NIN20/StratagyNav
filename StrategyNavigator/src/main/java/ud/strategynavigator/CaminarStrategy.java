3480c37c62413b7f27f57c2ee2d045e25ea5842d
02NIN20 <lycoronelp@udistrital.edu.co>
2025-05-16 14:55:08 -0500

    Update CaminarStrategy.java

--- StrategyNavigator/src/main/java/ud/strategynavigator/CaminarStrategy.java
+++ StrategyNavigator/src/main/java/ud/strategynavigator/CaminarStrategy.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ud.strategynavigator;
 
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author USUARIO
- */
 public class CaminarStrategy implements RouteStrategy{
 
    @Override
