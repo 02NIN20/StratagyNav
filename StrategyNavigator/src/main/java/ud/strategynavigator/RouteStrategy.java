ba373534ecc25a268e45b1231fd7732563304889
02NIN20 <lycoronelp@udistrital.edu.co>
2025-05-16 14:54:56 -0500

    Update RouteStrategy.java

--- StrategyNavigator/src/main/java/ud/strategynavigator/RouteStrategy.java
+++ StrategyNavigator/src/main/java/ud/strategynavigator/RouteStrategy.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package ud.strategynavigator;
 
-
-
-/**
- *
- * @author USUARIO
- */
 interface RouteStrategy {
     String getName();
     int calculateCost(int startX, int startY, int endX, int endY);
